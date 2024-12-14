e4fb8cc49f55b53f48beb3ec6955a85527416298
alexcbb <alexchapin@hotmail.fr>
2024-12-14 16:24:07 +0100

    Update env assert

--- libero/libero/envs/bddl_base_domain.py
+++ libero/libero/envs/bddl_base_domain.py
@@ -7,8 +7,12 @@ from robosuite.environments.manipulation.manipulation_env import ManipulationEnv
 from robosuite.models.tasks import ManipulationTask
 from robosuite.utils.placement_samplers import SequentialCompositeSampler
 from robosuite.utils.observables import Observable, sensor
-from robosuite.utils.mjcf_utils import CustomMaterial
-import robosuite.macros as macros
+from robosuite.robots.fixed_base_robot import FixedBaseRobot
+from robosuite.robots.mobile_robot import MobileRobot
+
+import time
+from libero.libero.envs.robots.single_arm import SingleArm
+from robosuite.utils.transform_utils import mat2quat
 
 import mujoco
 
@@ -30,11 +34,74 @@ def register_problem(target_class):
     """We design the mapping to be case-INsensitive."""
     TASK_MAPPING[target_class.__name__.lower()] = target_class
 
+class SingleArmEnv(ManipulationEnv):
+    """
+    A manipulation environment intended for a single robot arm.
+    """
 
-import time
+    def _load_model(self):
+        """
+        Verifies correct robot model is loaded
+        """
+        super()._load_model()
+
+        # Verify the correct robot has been loaded
+        assert isinstance(
+            self.robots[0], SingleArm
+        ), "Error: Expected one single-armed robot! Got {} type instead.".format(type(self.robots[0]))
+
+    def _check_robot_configuration(self, robots):
+        """
+        Sanity check to make sure the inputted robots and configuration is acceptable
+
+        Args:
+            robots (str or list of str): Robots to instantiate within this env
+        """
+        super()._check_robot_configuration(robots)
+        if type(robots) is list:
+            assert len(robots) == 1, "Error: Only one robot should be inputted for this task!"
+
+    @property
+    def _eef_xpos(self):
+        """
+        Grabs End Effector position
+
+        Returns:
+            np.array: End effector(x,y,z)
+        """
+        return np.array(self.sim.data.site_xpos[self.robots[0].eef_site_id])
+
+    @property
+    def _eef_xmat(self):
+        """
+        End Effector orientation as a rotation matrix
+        Note that this draws the orientation from the "ee" site, NOT the gripper site, since the gripper
+        orientations are inconsistent!
+
+        Returns:
+            np.array: (3,3) End Effector orientation matrix
+        """
+        pf = self.robots[0].gripper.naming_prefix
+
+        if self.env_configuration == "bimanual":
+            return np.array(self.sim.data.site_xmat[self.sim.model.site_name2id(pf + "right_grip_site")]).reshape(3, 3)
+        else:
+            return np.array(self.sim.data.site_xmat[self.sim.model.site_name2id(pf + "grip_site")]).reshape(3, 3)
+
+    @property
+    def _eef_xquat(self):
+        """
+        End Effector orientation as a (x,y,z,w) quaternion
+        Note that this draws the orientation from the "ee" site, NOT the gripper site, since the gripper
+        orientations are inconsistent!
+
+        Returns:
+            np.array: (x,y,z,w) End Effector quaternion
+        """
+        return mat2quat(self._eef_xmat)
 
 
-class BDDLBaseDomain(ManipulationEnv):
+class BDDLBaseDomain(SingleArmEnv):
     """
     A base domain for parsing bddl files.
     """
@@ -849,3 +916,19 @@ class BDDLBaseDomain(ManipulationEnv):
         ]:
             if object_name in query_dict:
                 return query_dict[object_name]
+            
+    def _check_robot_configuration(self, robots):
+        """
+        Sanity check to make sure inputted robots and the corresponding requested task/configuration combo is legal.
+        Should be implemented in every specific task module
+
+        Args:
+            robots (str or list of str): Inputted requested robots at the task-level environment
+        """
+        # Make sure all inputted robots are a manipulation robot
+        if type(robots) is str:
+            robots = [robots]
+        for robot in robots:
+            assert issubclass(ROBOT_CLASS_MAPPING[robot], FixedBaseRobot) or issubclass(
+                ROBOT_CLASS_MAPPING[robot], MobileRobot) or issubclass(ROBOT_CLASS_MAPPING[robot],
+                SingleArm), f"Only manipulator robots supported for manipulation environment! Got {ROBOT_CLASS_MAPPING[robot]}"
